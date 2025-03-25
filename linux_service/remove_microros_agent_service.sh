3c252f12ddf7cc94660a853bc61ae1bfadfbcd28
Gerard Harkema <gerardanneharkema@gmail.com>
2025-03-25 19:41:47 +0100

    20250325

--- linux_service/remove_microros_agent_service.sh
+++ linux_service/remove_microros_agent_service.sh
@@ -1,3 +1,3 @@
-systemctl stop microros_agent.service
-systemctl disable microros_agent.service
+sudo systemctl stop microros_agent.service
+sudo systemctl disable microros_agent.service
 sudo rm /etc/systemd/system/microros_agent.service
--- linux_service/remove_railtrack_gui_service.sh
+++ linux_service/remove_railtrack_gui_service.sh
@@ -1,3 +1,3 @@
-systemctl stop railtrack_gui.service
-systemctl disable railtrack_gui.service
+sudo systemctl stop railtrack_gui.service
+sudo systemctl disable railtrack_gui.service
 sudo rm /etc/systemd/system/railtrack_gui.service
