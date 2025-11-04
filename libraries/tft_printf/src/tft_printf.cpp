3fe382c9ac662d1bde8b7f1701cd0f2485637bc7
Gerard Harkema <GerardAnneHarkema@gmail.com>
2025-11-04 21:28:23 +0100

    20251104

--- libraries/tft_printf/src/tft_printf.cpp
+++ libraries/tft_printf/src/tft_printf.cpp
@@ -7,6 +7,7 @@ void tft_prinft_begin(Adafruit_ST7735 *printf_tft){
     printf_tft_p = printf_tft;
 }
 
+#if 0
 void tft_printf(int color, const char *fmt, ...){
   printf_tft_p->fillRect(0, 32, printf_tft_p->width()-1, printf_tft_p->height() - 32, ST77XX_BLACK);
   printf_tft_p->setCursor(1, 44);
@@ -48,4 +49,17 @@ void tft_printf(int color, const char *fmt, ...){
       }
   }
   va_end(args);
+}
+#endif
+void tft_printf(int color, const char *fmt, ...) {
+  char buffer[256];  // Pas grootte aan voor jouw display
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(buffer, sizeof(buffer), fmt, args);
+  va_end(args);
+
+  printf_tft_p->fillRect(0, 32, printf_tft_p->width() - 1, printf_tft_p->height() - 32, ST77XX_BLACK);
+  printf_tft_p->setCursor(1, 44);
+  printf_tft_p->setTextColor(color);
+  printf_tft_p->print(buffer);
 }
